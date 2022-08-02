3b5ef6a61ceb71db2acc74fac98782690737d3c1
Aaron Slaughter <aslaugh1@gmail.com>
2022-08-02 16:37:48 -0400

    create codegen files

--- brightspot-routing/app/generated/graphql.tsx
+++ brightspot-routing/app/generated/graphql.tsx
@@ -17,18 +17,33 @@ export type Scalars = {
   Long: any;
 };
 
+export type App = Content & Record_Interface & {
+  __typename?: 'App';
+  Page_app_connection?: Maybe<Page_App_Connection>;
+  _id?: Maybe<Scalars['ID']>;
+  cms_content?: Maybe<Content_ObjectModificationCmsContentField>;
+  title?: Maybe<Scalars['String']>;
+};
+
+
+export type AppPage_App_ConnectionArgs = {
+  limit?: InputMaybe<Scalars['Int']>;
+  offset?: InputMaybe<Scalars['Long']>;
+  sort?: InputMaybe<Page_Sort>;
+};
+
 export type Article = Content & Record_Interface & {
   __typename?: 'Article';
   _id?: Maybe<Scalars['ID']>;
+  body?: Maybe<Scalars['String']>;
   cms_content?: Maybe<Content_ObjectModificationCmsContentField>;
   headline?: Maybe<Scalars['String']>;
-  section?: Maybe<Section>;
-  text?: Maybe<Scalars['String']>;
+  page?: Maybe<Page>;
   url?: Maybe<Scalars['String']>;
 };
 
-export type Article_Section_Connection = {
-  __typename?: 'Article_section_connection';
+export type Article_Page_Connection = {
+  __typename?: 'Article_page_connection';
   items: Array<Article>;
   pageInfo?: Maybe<PageInfo>;
 };
@@ -69,16 +84,19 @@ export type Content_ObjectModificationCmsContentField = {
 
 export type Page = Content & Record_Interface & {
   __typename?: 'Page';
-  Section_page_connection?: Maybe<Section_Page_Connection>;
+  Article_page_connection?: Maybe<Article_Page_Connection>;
   _id?: Maybe<Scalars['ID']>;
+  app?: Maybe<App>;
   cms_content?: Maybe<Content_ObjectModificationCmsContentField>;
+  name?: Maybe<Scalars['String']>;
+  url?: Maybe<Scalars['String']>;
 };
 
 
-export type PageSection_Page_ConnectionArgs = {
+export type PageArticle_Page_ConnectionArgs = {
   limit?: InputMaybe<Scalars['Int']>;
   offset?: InputMaybe<Scalars['Long']>;
-  sort?: InputMaybe<Section_Sort>;
+  sort?: InputMaybe<Article_Sort>;
 };
 
 export type PageInfo = {
@@ -89,97 +107,18 @@ export type PageInfo = {
   offset?: Maybe<Scalars['Long']>;
 };
 
-export type Query = {
-  __typename?: 'Query';
-  Article?: Maybe<Article>;
-  Page?: Maybe<Page>;
-  Section?: Maybe<Section>;
-  _Entry?: Maybe<_Entry>;
-};
-
-
-export type QueryArticleArgs = {
-  id?: InputMaybe<Scalars['ID']>;
-  path?: InputMaybe<Scalars['String']>;
-};
-
-
-export type QueryPageArgs = {
-  id?: InputMaybe<Scalars['ID']>;
-  path?: InputMaybe<Scalars['String']>;
-};
-
-
-export type QuerySectionArgs = {
-  id?: InputMaybe<Scalars['ID']>;
-  path?: InputMaybe<Scalars['String']>;
-};
-
-
-export type Query_EntryArgs = {
-  id?: InputMaybe<Scalars['ID']>;
-  path?: InputMaybe<Scalars['String']>;
-};
-
-/** Represents a generic record. */
-export type Record = Record_Interface & {
-  __typename?: 'Record';
-  Article_section_connection?: Maybe<Article_Section_Connection>;
-  Section_page_connection?: Maybe<Section_Page_Connection>;
-  _id?: Maybe<Scalars['ID']>;
-  cms_content?: Maybe<Content_ObjectModificationCmsContentField>;
-};
-
-
-/** Represents a generic record. */
-export type RecordArticle_Section_ConnectionArgs = {
-  limit?: InputMaybe<Scalars['Int']>;
-  offset?: InputMaybe<Scalars['Long']>;
-  sort?: InputMaybe<Article_Sort>;
-};
-
-
-/** Represents a generic record. */
-export type RecordSection_Page_ConnectionArgs = {
-  limit?: InputMaybe<Scalars['Int']>;
-  offset?: InputMaybe<Scalars['Long']>;
-  sort?: InputMaybe<Section_Sort>;
-};
-
-/** Represents a generic record. */
-export type Record_Interface = {
-  _id?: Maybe<Scalars['ID']>;
-};
-
-export type Section = Content & Record_Interface & {
-  __typename?: 'Section';
-  Article_section_connection?: Maybe<Article_Section_Connection>;
-  _id?: Maybe<Scalars['ID']>;
-  cms_content?: Maybe<Content_ObjectModificationCmsContentField>;
-  name?: Maybe<Scalars['String']>;
-  page?: Maybe<Page>;
-  url?: Maybe<Scalars['String']>;
-};
-
-
-export type SectionArticle_Section_ConnectionArgs = {
-  limit?: InputMaybe<Scalars['Int']>;
-  offset?: InputMaybe<Scalars['Long']>;
-  sort?: InputMaybe<Article_Sort>;
-};
-
-export type Section_Page_Connection = {
-  __typename?: 'Section_page_connection';
-  items: Array<Section>;
+export type Page_App_Connection = {
+  __typename?: 'Page_app_connection';
+  items: Array<Page>;
   pageInfo?: Maybe<PageInfo>;
 };
 
-export type Section_Sort = {
-  option: Section_Sort_Option;
+export type Page_Sort = {
+  option: Page_Sort_Option;
   order?: InputMaybe<SortOrder>;
 };
 
-export enum Section_Sort_Option {
+export enum Page_Sort_Option {
   AverageTimeOnPage = 'AVERAGE_TIME_ON_PAGE',
   Bounces = 'BOUNCES',
   BounceRate = 'BOUNCE_RATE',
@@ -197,40 +136,138 @@ export enum Section_Sort_Option {
   UpdateDate = 'UPDATE_DATE'
 }
 
+export type Query = {
+  __typename?: 'Query';
+  App?: Maybe<App>;
+  Article?: Maybe<Article>;
+  Page?: Maybe<Page>;
+  _Entry?: Maybe<_Entry>;
+};
+
+
+export type QueryAppArgs = {
+  id?: InputMaybe<Scalars['ID']>;
+  path?: InputMaybe<Scalars['String']>;
+};
+
+
+export type QueryArticleArgs = {
+  id?: InputMaybe<Scalars['ID']>;
+  path?: InputMaybe<Scalars['String']>;
+};
+
+
+export type QueryPageArgs = {
+  id?: InputMaybe<Scalars['ID']>;
+  path?: InputMaybe<Scalars['String']>;
+};
+
+
+export type Query_EntryArgs = {
+  id?: InputMaybe<Scalars['ID']>;
+  path?: InputMaybe<Scalars['String']>;
+};
+
+/** Represents a generic record. */
+export type Record = Record_Interface & {
+  __typename?: 'Record';
+  _id?: Maybe<Scalars['ID']>;
+  cms_content?: Maybe<Content_ObjectModificationCmsContentField>;
+};
+
+/** Represents a generic record. */
+export type Record_Interface = {
+  _id?: Maybe<Scalars['ID']>;
+};
+
 export enum SortOrder {
   Ascending = 'ASCENDING',
   Descending = 'DESCENDING'
 }
 
-export type _Entry = Article | Page | Section;
+export type _Entry = App | Article | Page;
+
+export type GetAllPagesQueryVariables = Exact<{
+  path?: InputMaybe<Scalars['String']>;
+}>;
+
+
+export type GetAllPagesQuery = { __typename?: 'Query', App?: { __typename?: 'App', Page_app_connection?: { __typename?: 'Page_app_connection', items: Array<{ __typename?: 'Page', url?: string | null, name?: string | null, Article_page_connection?: { __typename?: 'Article_page_connection', items: Array<{ __typename?: 'Article', url?: string | null, headline?: string | null, body?: string | null, page?: { __typename?: 'Page', url?: string | null, name?: string | null } | null, cms_content?: { __typename?: 'Content_ObjectModificationCmsContentField', publishDate?: any | null } | null }> } | null }> } | null } | null };
 
 export type GetArticleQueryVariables = Exact<{
   path?: InputMaybe<Scalars['String']>;
 }>;
 
 
-export type GetArticleQuery = { __typename?: 'Query', Article?: { __typename?: 'Article', headline?: string | null, text?: string | null } | null };
+export type GetArticleQuery = { __typename?: 'Query', Article?: { __typename?: 'Article', headline?: string | null, body?: string | null } | null };
 
-export type GetSectionsQueryVariables = Exact<{
+export type GetAllArticlesQueryVariables = Exact<{
   path?: InputMaybe<Scalars['String']>;
 }>;
 
 
-export type GetSectionsQuery = { __typename?: 'Query', Page?: { __typename?: 'Page', Section_page_connection?: { __typename?: 'Section_page_connection', items: Array<{ __typename?: 'Section', name?: string | null, url?: string | null }> } | null } | null };
-
-export type GetArticlesQueryVariables = Exact<{
-  path?: InputMaybe<Scalars['String']>;
-}>;
+export type GetAllArticlesQuery = { __typename?: 'Query', Page?: { __typename?: 'Page', Article_page_connection?: { __typename?: 'Article_page_connection', items: Array<{ __typename?: 'Article', url?: string | null, headline?: string | null }> } | null } | null };
 
 
-export type GetArticlesQuery = { __typename?: 'Query', Section?: { __typename?: 'Section', name?: string | null, Article_section_connection?: { __typename?: 'Article_section_connection', items: Array<{ __typename?: 'Article', url?: string | null, headline?: string | null }> } | null } | null };
+export const GetAllPagesDocument = gql`
+    query GetAllPages($path: String = "") {
+  App(path: $path) {
+    Page_app_connection {
+      items {
+        url
+        name
+        Article_page_connection(sort: {option: PUBLISH_DATE, order: DESCENDING}) {
+          items {
+            page {
+              url
+              name
+            }
+            url
+            headline
+            body
+            cms_content {
+              publishDate
+            }
+          }
+        }
+      }
+    }
+  }
+}
+    `;
 
-
+/**
+ * __useGetAllPagesQuery__
+ *
+ * To run a query within a React component, call `useGetAllPagesQuery` and pass it any options that fit your needs.
+ * When your component renders, `useGetAllPagesQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useGetAllPagesQuery({
+ *   variables: {
+ *      path: // value for 'path'
+ *   },
+ * });
+ */
+export function useGetAllPagesQuery(baseOptions?: Apollo.QueryHookOptions<GetAllPagesQuery, GetAllPagesQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<GetAllPagesQuery, GetAllPagesQueryVariables>(GetAllPagesDocument, options);
+      }
+export function useGetAllPagesLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetAllPagesQuery, GetAllPagesQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<GetAllPagesQuery, GetAllPagesQueryVariables>(GetAllPagesDocument, options);
+        }
+export type GetAllPagesQueryHookResult = ReturnType<typeof useGetAllPagesQuery>;
+export type GetAllPagesLazyQueryHookResult = ReturnType<typeof useGetAllPagesLazyQuery>;
+export type GetAllPagesQueryResult = Apollo.QueryResult<GetAllPagesQuery, GetAllPagesQueryVariables>;
 export const GetArticleDocument = gql`
     query GetArticle($path: String = "") {
   Article(path: $path) {
     headline
-    text
+    body
   }
 }
     `;
@@ -262,51 +299,10 @@ export function useGetArticleLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions
 export type GetArticleQueryHookResult = ReturnType<typeof useGetArticleQuery>;
 export type GetArticleLazyQueryHookResult = ReturnType<typeof useGetArticleLazyQuery>;
 export type GetArticleQueryResult = Apollo.QueryResult<GetArticleQuery, GetArticleQueryVariables>;
-export const GetSectionsDocument = gql`
-    query GetSections($path: String = "") {
+export const GetAllArticlesDocument = gql`
+    query GetAllArticles($path: String = "") {
   Page(path: $path) {
-    Section_page_connection {
-      items {
-        name
-        url
-      }
-    }
-  }
-}
-    `;
-
-/**
- * __useGetSectionsQuery__
- *
- * To run a query within a React component, call `useGetSectionsQuery` and pass it any options that fit your needs.
- * When your component renders, `useGetSectionsQuery` returns an object from Apollo Client that contains loading, error, and data properties
- * you can use to render your UI.
- *
- * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
- *
- * @example
- * const { data, loading, error } = useGetSectionsQuery({
- *   variables: {
- *      path: // value for 'path'
- *   },
- * });
- */
-export function useGetSectionsQuery(baseOptions?: Apollo.QueryHookOptions<GetSectionsQuery, GetSectionsQueryVariables>) {
-        const options = {...defaultOptions, ...baseOptions}
-        return Apollo.useQuery<GetSectionsQuery, GetSectionsQueryVariables>(GetSectionsDocument, options);
-      }
-export function useGetSectionsLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetSectionsQuery, GetSectionsQueryVariables>) {
-          const options = {...defaultOptions, ...baseOptions}
-          return Apollo.useLazyQuery<GetSectionsQuery, GetSectionsQueryVariables>(GetSectionsDocument, options);
-        }
-export type GetSectionsQueryHookResult = ReturnType<typeof useGetSectionsQuery>;
-export type GetSectionsLazyQueryHookResult = ReturnType<typeof useGetSectionsLazyQuery>;
-export type GetSectionsQueryResult = Apollo.QueryResult<GetSectionsQuery, GetSectionsQueryVariables>;
-export const GetArticlesDocument = gql`
-    query GetArticles($path: String = "") {
-  Section(path: $path) {
-    name
-    Article_section_connection {
+    Article_page_connection {
       items {
         url
         headline
@@ -317,29 +313,29 @@ export const GetArticlesDocument = gql`
     `;
 
 /**
- * __useGetArticlesQuery__
+ * __useGetAllArticlesQuery__
  *
- * To run a query within a React component, call `useGetArticlesQuery` and pass it any options that fit your needs.
- * When your component renders, `useGetArticlesQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * To run a query within a React component, call `useGetAllArticlesQuery` and pass it any options that fit your needs.
+ * When your component renders, `useGetAllArticlesQuery` returns an object from Apollo Client that contains loading, error, and data properties
  * you can use to render your UI.
  *
  * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
  *
  * @example
- * const { data, loading, error } = useGetArticlesQuery({
+ * const { data, loading, error } = useGetAllArticlesQuery({
  *   variables: {
  *      path: // value for 'path'
  *   },
  * });
  */
-export function useGetArticlesQuery(baseOptions?: Apollo.QueryHookOptions<GetArticlesQuery, GetArticlesQueryVariables>) {
+export function useGetAllArticlesQuery(baseOptions?: Apollo.QueryHookOptions<GetAllArticlesQuery, GetAllArticlesQueryVariables>) {
         const options = {...defaultOptions, ...baseOptions}
-        return Apollo.useQuery<GetArticlesQuery, GetArticlesQueryVariables>(GetArticlesDocument, options);
+        return Apollo.useQuery<GetAllArticlesQuery, GetAllArticlesQueryVariables>(GetAllArticlesDocument, options);
       }
-export function useGetArticlesLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetArticlesQuery, GetArticlesQueryVariables>) {
+export function useGetAllArticlesLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetAllArticlesQuery, GetAllArticlesQueryVariables>) {
           const options = {...defaultOptions, ...baseOptions}
-          return Apollo.useLazyQuery<GetArticlesQuery, GetArticlesQueryVariables>(GetArticlesDocument, options);
+          return Apollo.useLazyQuery<GetAllArticlesQuery, GetAllArticlesQueryVariables>(GetAllArticlesDocument, options);
         }
-export type GetArticlesQueryHookResult = ReturnType<typeof useGetArticlesQuery>;
-export type GetArticlesLazyQueryHookResult = ReturnType<typeof useGetArticlesLazyQuery>;
-export type GetArticlesQueryResult = Apollo.QueryResult<GetArticlesQuery, GetArticlesQueryVariables>;
+export type GetAllArticlesQueryHookResult = ReturnType<typeof useGetAllArticlesQuery>;
+export type GetAllArticlesLazyQueryHookResult = ReturnType<typeof useGetAllArticlesLazyQuery>;
+export type GetAllArticlesQueryResult = Apollo.QueryResult<GetAllArticlesQuery, GetAllArticlesQueryVariables>;
