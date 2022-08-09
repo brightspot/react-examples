3b2f3c4f631c55a76bfc00e3739dc293d1b4e404
Aaron Slaughter <aslaugh1@gmail.com>
2022-08-09 16:16:35 -0400

    add date published to article view and add styles

--- brightspot-routing/app/components/Article/ArticleView.tsx
+++ brightspot-routing/app/components/Article/ArticleView.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import styles from './Article.module.css'
 import { Article } from '../../generated/graphql'
 
 interface Props {
@@ -7,9 +8,12 @@ interface Props {
 
 const ArticleView: NextPage<Props> = ({ article }) => {
   return (
-    <div>
-      <h2>{article.headline}</h2>
-      <p>{article.body}</p>
+    <div className={styles.articleWrapper}>
+      <h1 className={styles.articleHeadline}>{article.headline}</h1>
+      <p className={styles.datePublished}>
+        {new Date(article.cms_content?.publishDate).toLocaleString()}
+      </p>
+      <p className={styles.articleBody}>{article.body}</p>
     </div>
   )
 }
