b05753a589335c4f29d84ab86e78711f7ef4ef8a
Dan Slaughter <dan@brightspot.com>
2022-08-11 16:32:51 -0400

    Updates template scripts for brightspot app.
    Ensures there's a unique package.json name so that multiple apps can be uploaded to the same server.

--- .template/init.sh
+++ .template/init.sh
@@ -35,7 +35,33 @@ then
     exit 1
 fi
 
-mkdir $GIT_DIR/$1
-cp -r $GIT_DIR/.template/brightspot $GIT_DIR/$1/brightspot
+PACKAGE_NAME=$(echo "$NEW_FOLDER" | tr '-' '_')
+echo $PACKAGE_NAME
+
+# create new app dir
+mkdir $GIT_DIR/$NEW_FOLDER
+
+# create brightspot sub-folder and init package.json
+# we initially name it after the app name so npm init will use that as the package name
+# then we rename it back to brightspot
+mkdir $GIT_DIR/$NEW_FOLDER/$NEW_FOLDER
+cd $GIT_DIR/$NEW_FOLDER/$NEW_FOLDER
+npm init -y
+yarn add --dev typescript
+yarn add --dev @brightspot/cli
+cd -
+mv $GIT_DIR/$NEW_FOLDER/$NEW_FOLDER $GIT_DIR/$NEW_FOLDER/brightspot
+
+# create brightspot src folder
+mkdir -p $GIT_DIR/$NEW_FOLDER/brightspot/src/brightspot/example/$PACKAGE_NAME
+
+# copy brightspot default files
+cp $GIT_DIR/.template/brightspot/brightspot.json $GIT_DIR/$NEW_FOLDER/brightspot
+cp $GIT_DIR/.template/brightspot/tsconfig.json $GIT_DIR/$NEW_FOLDER/brightspot
+cp $GIT_DIR/.template/brightspot/README.md $GIT_DIR/$NEW_FOLDER/brightspot
+cp $GIT_DIR/.template/brightspot/brightspot-server-install.sh $GIT_DIR/$NEW_FOLDER/brightspot
+
+# copy app files
 cp -r $GIT_DIR/.template/apps/$2 $GIT_DIR/$1/app
+
 echo "Successfully initialized example [$1]."
