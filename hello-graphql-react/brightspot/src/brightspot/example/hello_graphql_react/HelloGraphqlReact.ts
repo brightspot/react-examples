1c76762b399f16251e34cb56cdae12cadffedda6
Dan Slaughter <dan@brightspot.com>
2022-08-29 06:57:08 -0400

    Makes name field indexed and unique.

--- hello-graphql-react/brightspot/src/brightspot/example/hello_graphql_react/HelloGraphqlReact.ts
+++ hello-graphql-react/brightspot/src/brightspot/example/hello_graphql_react/HelloGraphqlReact.ts
@@ -2,12 +2,14 @@ import JavaClass from 'brightspot-types/JavaClass'
 import JavaField from 'brightspot-types/JavaField'
 import DisplayName from 'brightspot-types/com/psddev/dari/db/Recordable$DisplayName'
 import Content from 'brightspot-types/com/psddev/cms/db/Content'
+import Indexed from 'brightspot-types/com/psddev/dari/db/Recordable$Indexed'
 
 @DisplayName({ value: 'Hello GraphQL React' })
 export default class HelloGraphqlReact extends JavaClass(
   'brightspot.example.hello_graphql_react.HelloGraphqlReact',
   Content
 ) {
+  @Indexed({ unique: true })
   @JavaField(String)
   name?: string
 
