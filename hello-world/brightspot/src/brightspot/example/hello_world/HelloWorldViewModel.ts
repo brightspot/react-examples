8c8fafd72fcc634283ae5df472baeb8c1d96e662
Dan Slaughter <dan@brightspot.com>
2022-09-02 13:13:50 -0400

    Removes unnecessary interface.

--- hello-world/brightspot/src/brightspot/example/hello_world/HelloWorldViewModel.ts
+++ hello-world/brightspot/src/brightspot/example/hello_world/HelloWorldViewModel.ts
@@ -4,7 +4,6 @@ import JavaMethodReturn from 'brightspot-types/JavaMethodReturn'
 
 import ViewInterface from 'brightspot-types/com/psddev/cms/view/ViewInterface'
 import ViewModel from 'brightspot-types/com/psddev/cms/view/ViewModel'
-import PageEntryView from 'brightspot-types/com/psddev/cms/view/PageEntryView'
 
 import HelloWorldEndpoint from './HelloWorldEndpoint'
 
@@ -12,7 +11,6 @@ import HelloWorldEndpoint from './HelloWorldEndpoint'
 export default class HelloBrightspotViewModel extends JavaClass(
   'brightspot.example.hello_world.HelloWorldViewModel',
   ViewModel.Of(HelloWorldEndpoint),
-  PageEntryView
 ) {
   @JavaMethodParameters()
   @JavaMethodReturn(String)
