81edc9aa29acc12a77add76a206bac3fe8f12449
Roderick Cardenas <roderickcardenas1@gmail.com>
2022-09-10 13:35:15 -0400

    Update prettier formatting

--- graphql-restification/app/src/index.tsx
+++ graphql-restification/app/src/index.tsx
@@ -1,25 +1,28 @@
-import App from './app';
-import ReactDOM from 'react-dom/client';
-import { ApolloClient, ApolloProvider,InMemoryCache, createHttpLink } from '@apollo/client';
+import App from './app'
+import ReactDOM from 'react-dom/client'
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from '@apollo/client'
 
 const link = createHttpLink({
   uri: process.env.REACT_APP_GRAPHQL_URL ?? '',
   credentials: 'same-origin',
   headers: {
     'X-Client-ID': process.env.REACT_APP_GRAPHQL_CLIENT_ID,
-    'X-Client-Secret': process.env.REACT_APP_GRAPHQL_CLIENT_SECRET
-  }
+    'X-Client-Secret': process.env.REACT_APP_GRAPHQL_CLIENT_SECRET,
+  },
 })
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link
-});
+  link,
+})
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>
-);
+)
