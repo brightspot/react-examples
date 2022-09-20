4f74de27460dcc2e5f2dd56cbad6b8eed8558612
Roderick Cardenas <roderickcardenas1@gmail.com>
2022-09-20 12:08:40 -0400

    intialize

---
+++ restification/app/src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: process.env.REACT_APP_GRAPHQL_URL ?? '',
+  cache: new InMemoryCache(),
+})
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+)
