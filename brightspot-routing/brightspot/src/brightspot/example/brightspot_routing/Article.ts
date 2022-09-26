24e9ef7a44d1d7b76d515b392308eb305237ee65
Aaron Slaughter <aslaugh1@gmail.com>
2022-09-26 13:54:23 -0400

    create models

---
+++ brightspot-routing/brightspot/src/brightspot/example/brightspot_routing/Article.ts
@@ -0,0 +1,35 @@
+import JavaClass from 'brightspot-types/JavaClass'
+import JavaField from 'brightspot-types/JavaField'
+
+import Content from 'brightspot-types/com/psddev/cms/db/Content'
+import DirectoryItem from 'brightspot-types/com/psddev/cms/db/Directory$Item'
+import DisplayName from 'brightspot-types/com/psddev/dari/db/Recordable$DisplayName'
+import Indexed from 'brightspot-types/com/psddev/dari/db/Recordable$Indexed'
+import Required from 'brightspot-types/com/psddev/dari/db/Recordable$Required'
+import Site from 'brightspot-types/com/psddev/cms/db/Site'
+import Utils from 'brightspot-types/com/psddev/dari/util/Utils'
+
+import Section from './Section'
+
+@DisplayName({ value: 'Brightspot Routing Article' })
+export default class Article extends JavaClass(
+  'brightspot.example.brightspot_routing',
+  Content,
+  DirectoryItem
+) {
+  @JavaField(String)
+  @Required
+  headline?: string
+
+  @JavaField(String)
+  body?: string
+
+  @JavaField(Section)
+  @Indexed
+  @Required
+  section?: Section;
+
+  [`createPermalink(com.psddev.cms.db.Site)`](site: Site): string {
+    return Utils.toNormalized(this.headline)
+  }
+}
---
+++ brightspot-routing/brightspot/src/brightspot/example/brightspot_routing/Section.ts
@@ -0,0 +1,22 @@
+import JavaClass from 'brightspot-types/JavaClass'
+import JavaField from 'brightspot-types/JavaField'
+
+import Content from 'brightspot-types/com/psddev/cms/db/Content'
+import DirectoryItem from 'brightspot-types/com/psddev/cms/db/Directory$Item'
+import DisplayName from 'brightspot-types/com/psddev/dari/db/Recordable$DisplayName'
+import Site from 'brightspot-types/com/psddev/cms/db/Site'
+import Utils from 'brightspot-types/com/psddev/dari/util/Utils'
+
+@DisplayName({ value: 'Brightspot Routing Section' })
+export default class Section extends JavaClass(
+  'brightspot.example.brightspot_routing.Section',
+  Content,
+  DirectoryItem
+) {
+  @JavaField(String)
+  name?: string;
+
+  [`createPermalink(com.psddev.cms.db.Site)`](site: Site): string {
+    return Utils.toNormalized(this.name)
+  }
+}
