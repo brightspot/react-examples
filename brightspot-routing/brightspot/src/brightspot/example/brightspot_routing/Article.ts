3b36ccd7107d5a0c535d020f382e27d47a7e694f
Aaron Slaughter <aslaugh1@gmail.com>
2022-09-27 11:51:08 -0400

    make permalink field required

--- brightspot-routing/brightspot/src/brightspot/example/brightspot_routing/Article.ts
+++ brightspot-routing/brightspot/src/brightspot/example/brightspot_routing/Article.ts
@@ -5,6 +5,7 @@ import Content from 'brightspot-types/com/psddev/cms/db/Content'
 import DirectoryItem from 'brightspot-types/com/psddev/cms/db/Directory$Item'
 import DisplayName from 'brightspot-types/com/psddev/dari/db/Recordable$DisplayName'
 import Indexed from 'brightspot-types/com/psddev/dari/db/Recordable$Indexed'
+import ReadOnly from 'brightspot-types/com/psddev/cms/db/ToolUi$ReadOnly'
 import Required from 'brightspot-types/com/psddev/dari/db/Recordable$Required'
 import Site from 'brightspot-types/com/psddev/cms/db/Site'
 import Utils from 'brightspot-types/com/psddev/dari/util/Utils'
@@ -28,6 +29,16 @@ export default class Article extends JavaClass(
   @Indexed
   @Required
   section?: Section;
+
+  @JavaField(String)
+  @Indexed({ unique: true })
+  @Required
+  @ReadOnly
+  url?: string
+
+  beforeSave(): void {
+    this.url = this.getPermalink()
+  }
 
   [`createPermalink(com.psddev.cms.db.Site)`](site: Site): string {
     return Utils.toNormalized(this.headline)
--- brightspot-routing/brightspot/src/brightspot/example/brightspot_routing/Section.ts
+++ brightspot-routing/brightspot/src/brightspot/example/brightspot_routing/Section.ts
@@ -4,6 +4,9 @@ import JavaField from 'brightspot-types/JavaField'
 import Content from 'brightspot-types/com/psddev/cms/db/Content'
 import DirectoryItem from 'brightspot-types/com/psddev/cms/db/Directory$Item'
 import DisplayName from 'brightspot-types/com/psddev/dari/db/Recordable$DisplayName'
+import Indexed from 'brightspot-types/com/psddev/dari/db/Recordable$Indexed'
+import ReadOnly from 'brightspot-types/com/psddev/cms/db/ToolUi$ReadOnly'
+import Required from 'brightspot-types/com/psddev/dari/db/Recordable$Required'
 import Site from 'brightspot-types/com/psddev/cms/db/Site'
 import Utils from 'brightspot-types/com/psddev/dari/util/Utils'
 
@@ -15,6 +18,16 @@ export default class Section extends JavaClass(
 ) {
   @JavaField(String)
   name?: string;
+
+  @JavaField(String)
+  @Indexed({ unique: true })
+  @Required
+  @ReadOnly
+  url?: string
+
+  beforeSave(): void {
+    this.url = this.getPermalink()
+  }
 
   [`createPermalink(com.psddev.cms.db.Site)`](site: Site): string {
     return Utils.toNormalized(this.name)
