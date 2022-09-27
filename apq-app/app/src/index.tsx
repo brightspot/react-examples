e2c49081052f8c9131e5dfae42df3ee2780c7463
Mandi Haase <haase1020@gmail.com>
2022-09-26 21:07:01 -0400

    confirm that hash works is getPersistedQueryHashKey name is sha256Hash

--- apq-app/app/src/index.tsx
+++ apq-app/app/src/index.tsx
@@ -8,26 +8,26 @@ import {
   DocumentNode,
 } from '@apollo/client'
 import { createPersistedQueryLink } from '@apollo/client/link/persisted-queries'
-import { sha256, sha1 } from 'crypto-hash'
-import crypto from 'crypto'
+import { 
+  // sha256, 
+  sha1 
+} from 'crypto-hash'
 
 import { print } from 'graphql/language/printer'
 
 const httpLink = new HttpLink({
   uri: process.env.REACT_APP_GRAPHQL_URL ?? '',
-  includeExtensions: true,
 })
 
 const persistedQueriesLink = createPersistedQueryLink({
   // sha256,
   generateHash: async (schema: DocumentNode) => {
-    console.log(print(schema))
     const result = await sha1(print(schema))
-    console.log('result', result)
     return result
   },
   useGETForHashedQueries: true,
 })
+
 const client = new ApolloClient({
   cache: new InMemoryCache(),
   // uri: process.env.REACT_APP_GRAPHQL_URL ?? '',
--- apq-app/brightspot/src/brightspot/example/apq/FooHashAlgorithm.ts
+++ apq-app/brightspot/src/brightspot/example/apq/FooHashAlgorithm.ts
@@ -1,11 +1,8 @@
 import AutomaticPersistedQueryHashAlgorithm from 'brightspot-types/com/psddev/graphql/pqp/AutomaticPersistedQueryHashAlgorithm'
-// import Hashing from 'brightspot-types/com/google/common/hash/Hashing';
 import Utils from 'brightspot-types/com/psddev/dari/util/Utils'
 import JavaClass from 'brightspot-types/JavaClass'
 import JavaMethodReturn from 'brightspot-types/JavaMethodReturn'
 import JavaMethodParameters from 'brightspot-types/JavaMethodParameters'
-// import UtilsOverride from 'brightspot-types/com/psddev/dari/util/UtilsOverride'
-// import PersistedQueryExtension from 'brightspot-types/com/psddev/graphql/pqp/PersistedQueryExtension'
 
 export default class FooHashAlgorithm extends JavaClass(
   'brightspot.example.apq.FooHashAlgorithm',
@@ -14,37 +11,17 @@ export default class FooHashAlgorithm extends JavaClass(
   @JavaMethodReturn(String)
   @JavaMethodParameters()
   getPersistedQueryHashKey(): string {
-    return 'sha1Hash'
+    return 'sha256Hash' // set string at sha256Hash since Apollo sends all hashs in sha256Hash field
   }
-  // [`getPersistedQueryHashKey()`](): string {
-  //   console.log('FOO HASH ✨ ✨ ✨', )
-  //   // return PersistedQueryExtension.SHA_256_HASH_KEY
-  //   return 'md5Hash'
-  // }
 
   @JavaMethodReturn(String)
   @JavaMethodParameters(String, String)
   calculateHash(sharedSecret: string, query: string): string {
     const hashedValue = Utils['hash(java.lang.String,java.lang.String)'](
-      'SHA1',
+      'SHA-1',
       query
     )
     const result = Utils.hex(hashedValue)
-    // console.log('RESULT ✨ ✨ ✨', result)
-
     return result
   }
-  // [`calculateHash(java.lang.String,java.lang.String)`](
-  //   sharedSecret: string,
-  //   query: string
-  // ): string {
-  //   const hashedValue = Utils['hash(java.lang.String,java.lang.String)'](
-  //     'MD5',
-  //     query
-  //   )
-  //   const result = Utils.hex(hashedValue)
-  //   // console.log('RESULT ✨ ✨ ✨', result)
-
-  //   return result
-  // }
 }
