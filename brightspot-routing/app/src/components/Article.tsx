5be3805027cc5b9e852f69fe9026a019c36cbb7a
Aaron Slaughter <aslaugh1@gmail.com>
2022-09-28 08:39:12 -0400

    remove excess null checks

--- brightspot-routing/app/src/components/Article.tsx
+++ brightspot-routing/app/src/components/Article.tsx
@@ -14,29 +14,29 @@ const Article = () => {
 
   if (error) console.log(error.message)
   if (loading) return <div className="loading">loading...</div>
-  if (!data?.Article && !loading) return <NotFound />
+  if (!data?.Article) return <NotFound />
 
   // removes leading '/' when comparing to url param
-  if (data?.Article?.section?.path?.slice(1) !== section) return <NotFound />
+  if (data.Article.section?.path?.slice(1) !== section) return <NotFound />
 
   // redirects to new URL if user navigated to old URL
-  if (data?.Article?.path?.slice(1) !== article) {
-    navigate(`${data?.Article?.section?.path}${data?.Article?.path}`)
+  if (data?.Article.path?.slice(1) !== article) {
+    navigate(`${data.Article.section?.path}${data.Article.path}`)
   }
 
-  let publishDate = new Date(data?.Article?.publishDate || 0)
+  let publishDate = new Date(data.Article.publishDate || 0)
 
   return (
     <div className="container">
-      <h1 className="article-headline">{data?.Article?.headline}</h1>
+      <h1 className="article-headline">{data.Article.headline}</h1>
       <span className="article-datePublished">
         {publishDate.toDateString() + ' - ' + publishDate.toTimeString()}
       </span>
       <br />
-      <Link to={`${data?.Article?.section?.path}`}>
-        <p className="article-sectionName">{data?.Article?.section?.name}</p>
+      <Link to={`${data.Article.section?.path}`}>
+        <p className="article-sectionName">{data.Article.section?.name}</p>
       </Link>
-      <p className="article-body">{data?.Article?.body}</p>
+      <p className="article-body">{data.Article.body}</p>
     </div>
   )
 }
