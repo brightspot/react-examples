ba6d6928f0323894b3f716ccdef932f2f90eb914
Mandi Haase <haase1020@gmail.com>
2022-09-29 11:39:18 -0400

    change App.tsx to use const

--- app-routing/app/src/App.tsx
+++ app-routing/app/src/App.tsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom'
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 
-function App() {
+const App = () => {
   return (
     <div className="app">
       <Navbar />
