0364160141167b3e2903307a8a73d1601a2e1efb
Aaron Slaughter <aslaugh1@gmail.com>
2022-09-30 11:32:09 -0400

    simplify return statement

--- brightspot-routing/app/src/components/List.tsx
+++ brightspot-routing/app/src/components/List.tsx
@@ -5,22 +5,20 @@ type Props = {
   section: Section
 }
 
-const List = ({ section }: Props) => {
-  return (
-    <section className="cardList-section">
-      {section.articles &&
-        section.articles.map((article, i) => (
-          <Link key={i} to={`${section.path}${article?.path}`}>
-            <div className="list-item" data-first={i === 0 ? true : null}>
-              {article?.section?.name && (
-                <p className="cardList-pageName">{article?.section?.name}</p>
-              )}
-              <h2 className="cardList-articleHeadline">{article?.headline}</h2>
-            </div>
-          </Link>
-        ))}
-    </section>
-  )
-}
+const List = ({ section }: Props) => (
+  <section className="cardList-section">
+    {section.articles &&
+      section.articles.map((article, i) => (
+        <Link key={i} to={`${section.path}${article?.path}`}>
+          <div className="list-item" data-first={i === 0 ? true : null}>
+            {article?.section?.name && (
+              <p className="cardList-pageName">{article?.section?.name}</p>
+            )}
+            <h2 className="cardList-articleHeadline">{article?.headline}</h2>
+          </div>
+        </Link>
+      ))}
+  </section>
+)
 
 export default List
