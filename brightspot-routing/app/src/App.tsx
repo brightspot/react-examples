f0de24472632ef4ed76c819355133292ecaf0757
Aaron Slaughter <aslaugh1@gmail.com>
2022-09-30 11:32:39 -0400

    update component to use arrow function

--- brightspot-routing/app/src/App.tsx
+++ brightspot-routing/app/src/App.tsx
@@ -4,16 +4,14 @@ import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 
-function App() {
-  return (
-    <div className="app">
-      <Navbar />
-      <main>
-        <Outlet />
-      </main>
-      <Footer />
-    </div>
-  )
-}
+const App = () => (
+  <div className="app">
+    <Navbar />
+    <main>
+      <Outlet />
+    </main>
+    <Footer />
+  </div>
+)
 
 export default App
