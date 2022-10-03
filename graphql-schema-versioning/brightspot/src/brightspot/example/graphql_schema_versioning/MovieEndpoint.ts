c09bf62d257e7106408c698a2029a19e94f8e8b4
Roderick Cardenas <roderickcardenas1@gmail.com>
2022-10-03 14:51:05 -0400

    clean up imports

--- graphql-schema-versioning/brightspot/src/brightspot/example/graphql_schema_versioning/MovieEndpoint.ts
+++ graphql-schema-versioning/brightspot/src/brightspot/example/graphql_schema_versioning/MovieEndpoint.ts
@@ -1,11 +1,12 @@
+import JavaSet from 'brightspot-types/java/util/Set'
+import List from 'brightspot-types/java/util/List'
+
 import ContentDeliveryApiEndpoint from 'brightspot-types/com/psddev/graphql/cda/ContentDeliveryApiEndpoint'
 import ContentDeliveryEntryPointField from 'brightspot-types/com/psddev/graphql/cda/ContentDeliveryEntryPointField'
 import GraphQLApiAccessOption from 'brightspot-types/com/psddev/graphql/GraphQLApiAccessOption'
 import GraphQLApiAccessOptionImplicit from 'brightspot-types/com/psddev/graphql/GraphQLApiAccessOptionImplicit'
 import GraphQLCorsConfiguration from 'brightspot-types/com/psddev/graphql/GraphQLCorsConfiguration'
 import JavaClass from 'brightspot-types/JavaClass'
-import JavaSet from 'brightspot-types/java/util/Set'
-import List from 'brightspot-types/java/util/List'
 import Singleton from 'brightspot-types/com/psddev/dari/db/Singleton'
 
 import MovieViewModel from './MovieViewModel'
