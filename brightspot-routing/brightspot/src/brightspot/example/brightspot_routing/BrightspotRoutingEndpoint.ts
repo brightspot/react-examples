008686d79cf8f08cf11a6bd58867cb7ba70a5033
Aaron Slaughter <aslaugh1@gmail.com>
2022-10-12 18:12:38 -0400

    renamed view models

--- brightspot-routing/brightspot/src/brightspot/example/brightspot_routing/BrightspotRoutingEndpoint.ts
+++ brightspot-routing/brightspot/src/brightspot/example/brightspot_routing/BrightspotRoutingEndpoint.ts
@@ -3,16 +3,17 @@ import JavaClass from 'brightspot-types/JavaClass'
 import JavaSet from 'brightspot-types/java/util/Set'
 import List from 'brightspot-types/java/util/List'
 
-import ContentDeliveryApiAccessOption from 'brightspot-types/com/psddev/graphql/cda/ContentDeliveryApiAccessOption'
-import ContentDeliveryApiAccessOptionImplicit from 'brightspot-types/com/psddev/graphql/cda/ContentDeliveryApiAccessOptionImplicit'
 import ContentDeliveryApiEndpoint from 'brightspot-types/com/psddev/graphql/cda/ContentDeliveryApiEndpoint'
 import ContentDeliveryEntryPointField from 'brightspot-types/com/psddev/graphql/cda/ContentDeliveryEntryPointField'
+import GraphQLApiAccessOption from 'brightspot-types/com/psddev/graphql/GraphQLApiAccessOption'
+import GraphQLApiAccessOptionImplicit from 'brightspot-types/com/psddev/graphql/GraphQLApiAccessOptionImplicit'
 import GraphQLCorsConfiguration from 'brightspot-types/com/psddev/graphql/GraphQLCorsConfiguration'
 import Singleton from 'brightspot-types/com/psddev/dari/db/Singleton'
+
+import AllArticlesViewModel from './AllArticlesViewModel'
+import AllSectionsViewModel from './AllSectionsViewModel'
 import ArticleViewModel from './ArticleViewModel'
 import SectionViewModel from './SectionViewModel'
-import SectionsViewModel from './SectionsViewModel'
-import ArticlesViewModel from './ArticlesViewModel'
 
 export default class BrightspotRoutingEndpoint extends JavaClass(
   'brightspot.example.brightspot_routing.BrightspotRoutingEndpoint',
@@ -25,12 +26,13 @@ export default class BrightspotRoutingEndpoint extends JavaClass(
     ] as unknown as JavaSet<string>
   }
 
-  [`getQueryEntryFields()`](): List<ContentDeliveryEntryPointField> {
+  getQueryEntryFields(): List<ContentDeliveryEntryPointField> {
     return [
+      AllArticlesViewModel.class as Class<AllArticlesViewModel>,
+      AllSectionsViewModel.class as Class<AllSectionsViewModel>,
       ArticleViewModel.class as Class<ArticleViewModel>,
-      ArticlesViewModel.class as Class<ArticlesViewModel>,
       SectionViewModel.class as Class<SectionViewModel>,
-      SectionsViewModel.class as Class<SectionsViewModel>,
+      // TODO: add marker interface
     ].map(
       (c) => new ContentDeliveryEntryPointField(c)
     ) as unknown as List<ContentDeliveryEntryPointField>
@@ -40,7 +42,7 @@ export default class BrightspotRoutingEndpoint extends JavaClass(
     corsConfiguration.addAllowedOrigin('localhost')
   }
 
-  getAccessOption(): ContentDeliveryApiAccessOption {
-    return new ContentDeliveryApiAccessOptionImplicit()
+  getApiAccessOption(): GraphQLApiAccessOption {
+    return new GraphQLApiAccessOptionImplicit()
   }
 }
