00280d5d96750c6b6f67b7d6c4c59e764a5a6bd2
Roderick Cardenas <roderickcardenas1@gmail.com>
2022-10-13 16:11:33 -0400

    Update imports

--- marked-text/brightspot/src/brightspot/example/marked_text/ArticleEndpoint.ts
+++ marked-text/brightspot/src/brightspot/example/marked_text/ArticleEndpoint.ts
@@ -5,10 +5,10 @@ import List from 'brightspot-types/java/util/List'
 
 import ContentDeliveryApiEndpoint from 'brightspot-types/com/psddev/graphql/cda/ContentDeliveryApiEndpoint'
 import ContentDeliveryEntryPointField from 'brightspot-types/com/psddev/graphql/cda/ContentDeliveryEntryPointField'
-import RecordableDeliveryEntryPointField from 'brightspot-types/com/psddev/graphql/cda/rda/RecordableDeliveryEntryPointField'
 import GraphQLApiAccessOption from 'brightspot-types/com/psddev/graphql/GraphQLApiAccessOption'
 import GraphQLApiAccessOptionImplicit from 'brightspot-types/com/psddev/graphql/GraphQLApiAccessOptionImplicit'
 import GraphQLCorsConfiguration from 'brightspot-types/com/psddev/graphql/GraphQLCorsConfiguration'
+import RecordableDeliveryEntryPointField from 'brightspot-types/com/psddev/graphql/cda/rda/RecordableDeliveryEntryPointField'
 import Singleton from 'brightspot-types/com/psddev/dari/db/Singleton'
 
 import Article from './Article'
