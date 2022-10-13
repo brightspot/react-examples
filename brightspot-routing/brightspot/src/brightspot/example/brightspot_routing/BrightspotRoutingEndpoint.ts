d5abf2f6f56454c4c6db1fcad24f4375eab8d6ae
Aaron Slaughter <aslaugh1@gmail.com>
2022-10-13 09:06:34 -0400

    add updateCorsConfiguration

--- brightspot-routing/brightspot/src/brightspot/example/brightspot_routing/BrightspotRoutingEndpoint.ts
+++ brightspot-routing/brightspot/src/brightspot/example/brightspot_routing/BrightspotRoutingEndpoint.ts
@@ -39,6 +39,7 @@ export default class BrightspotRoutingEndpoint extends JavaClass(
   }
 
   updateCorsConfiguration(corsConfiguration: GraphQLCorsConfiguration): void {
+    super.updateCorsConfiguration(corsConfiguration)
     corsConfiguration.addAllowedOrigin('localhost')
   }
 
