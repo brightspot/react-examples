a1c1fe56a3bf0db8f8aef58a17628a6bd95de70e
Roderick Cardenas <roderickcardenas1@gmail.com>
2022-10-13 16:11:44 -0400

    update imports and method

--- marked-text/brightspot/src/brightspot/example/marked_text/Article.ts
+++ marked-text/brightspot/src/brightspot/example/marked_text/Article.ts
@@ -1,14 +1,13 @@
 import JavaClass from 'brightspot-types/JavaClass'
 import JavaField from 'brightspot-types/JavaField'
+import JavaRequired from 'brightspot-types/com/psddev/dari/db/Recordable$Required'
 
 import Content from 'brightspot-types/com/psddev/cms/db/Content'
 import DirectoryItem from 'brightspot-types/com/psddev/cms/db/Directory$Item'
-import JavaRequired from 'brightspot-types/com/psddev/dari/db/Recordable$Required'
+import GuideFieldRichTextToolbar from 'brightspot-types/com/psddev/cms/rte/GuideFieldRichTextToolbar'
 import RichText from 'brightspot-types/com/psddev/cms/db/ToolUi$RichText'
 import Site from 'brightspot-types/com/psddev/cms/db/Site'
 import Utils from 'brightspot-types/com/psddev/dari/util/Utils'
-
-import GuideFieldRichTextToolbar from 'brightspot-types/com/psddev/cms/rte/GuideFieldRichTextToolbar'
 
 export default class Article extends JavaClass(
   'brightspot.example.marked_text.Article',
@@ -27,9 +26,9 @@ export default class Article extends JavaClass(
     toolbar: GuideFieldRichTextToolbar.class,
     lines: 5,
   })
-  body?: string
+  body?: string;
 
-  createPermalink(site: Site): string {
+  [`createPermalink(com.psddev.cms.db.Site)`](site: Site): string {
     return Utils.toNormalized(this.headline)
   }
 }
