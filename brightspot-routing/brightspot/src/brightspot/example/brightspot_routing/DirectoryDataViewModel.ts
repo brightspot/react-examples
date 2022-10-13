47d8515296be5cb870518c6bbd9e9220e2c02580
Aaron Slaughter <aslaugh1@gmail.com>
2022-10-13 09:07:09 -0400

    organize imports

--- brightspot-routing/brightspot/src/brightspot/example/brightspot_routing/DirectoryDataViewModel.ts
+++ brightspot-routing/brightspot/src/brightspot/example/brightspot_routing/DirectoryDataViewModel.ts
@@ -1,13 +1,13 @@
 import JavaClass from 'brightspot-types/JavaClass'
 import JavaMethodParameters from 'brightspot-types/JavaMethodParameters'
 import JavaMethodReturn from 'brightspot-types/JavaMethodReturn'
+import JavaSet from 'brightspot-types/java/util/Set'
 
 import DirectoryData from 'brightspot-types/com/psddev/cms/db/Directory$Data'
 import ViewInterface from 'brightspot-types/com/psddev/cms/view/ViewInterface'
 import ViewModel from 'brightspot-types/com/psddev/cms/view/ViewModel'
 
 import DirectoryPathViewModel from './DirectoryPathViewModel'
-import JavaSet from 'brightspot-types/java/util/Set'
 
 @ViewInterface
 export default class DirectoryDataViewModel extends JavaClass(
