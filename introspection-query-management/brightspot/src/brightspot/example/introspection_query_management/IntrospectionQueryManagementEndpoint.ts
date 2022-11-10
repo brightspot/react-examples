166541da49d3d069708873d543522c3689244583
Aaron Slaughter <aslaugh1@gmail.com>
2022-11-10 14:52:58 -0500

    create endpoint with introspection rule

---
+++ introspection-query-management/brightspot/src/brightspot/example/introspection_query_management/IntrospectionQueryManagementEndpoint.ts
@@ -0,0 +1,61 @@
+import JavaClass from 'brightspot-types/JavaClass'
+import JavaField from 'brightspot-types/JavaField'
+import JavaSet from 'brightspot-types/java/util/Set'
+import List from 'brightspot-types/java/util/List'
+
+import ClassFinder from 'brightspot-types/com/psddev/dari/util/ClassFinder'
+import ContentDeliveryApiEndpoint from 'brightspot-types/com/psddev/graphql/cda/ContentDeliveryApiEndpoint'
+import ContentDeliveryEntryPointField from 'brightspot-types/com/psddev/graphql/cda/ContentDeliveryEntryPointField'
+import GraphQLApiAccessOption from 'brightspot-types/com/psddev/graphql/GraphQLApiAccessOption'
+import GraphQLApiAccessOptionImplicit from 'brightspot-types/com/psddev/graphql/GraphQLApiAccessOptionImplicit'
+import GraphQLCorsConfiguration from 'brightspot-types/com/psddev/graphql/GraphQLCorsConfiguration'
+import IntrospectionQueryRule from 'brightspot-types/com/psddev/graphql/IntrospectionQueryRule'
+import Singleton from 'brightspot-types/com/psddev/dari/db/Singleton'
+
+import AllSongsViewModel from './AllSongsViewModel'
+
+export default class IntrospectionQueryManagementEndpoint extends JavaClass(
+  'brightspot.example.introspection_query_management',
+  ContentDeliveryApiEndpoint,
+  Singleton
+) {
+  @JavaField(String)
+  introspectionKey?: string
+
+  afterCreate(): void {
+    this.introspectionKey = 'abcdefghijklmnopqrstuvwxyz'
+  }
+
+  getPaths(): JavaSet<string> {
+    return [
+      '/graphql/delivery/introspection-query-management',
+    ] as unknown as JavaSet<string>
+  }
+
+  [`getQueryEntryFields()`](): List<ContentDeliveryEntryPointField> {
+    return [AllSongsViewModel.getClass()].map(
+      (c) => new ContentDeliveryEntryPointField(c)
+    ) as unknown as List<ContentDeliveryEntryPointField>
+  }
+
+  updateCorsConfiguration(corsConfiguration: GraphQLCorsConfiguration): void {
+    super.updateCorsConfiguration(corsConfiguration)
+    corsConfiguration.addAllowedOrigin('localhost')
+    corsConfiguration.addAllowedOrigin('cloud.hasura.io') // for testing externally with hasura.io
+    corsConfiguration.addAllowedHeader('Introspection-Key')
+  }
+
+  getApiAccessOption(): GraphQLApiAccessOption {
+    return new GraphQLApiAccessOptionImplicit()
+  }
+
+  getIntrospectionQueryRule(): IntrospectionQueryRule {
+    const introspectionQueryRule = ClassFinder.getClass(
+      'brightspot.example.introspection_query_management.ExampleIntrospectionQueryRule'
+    )
+    let rule = new introspectionQueryRule()
+    rule.introspectionKey = this.introspectionKey
+
+    return rule
+  }
+}
