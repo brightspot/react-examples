1e4b672f096bb882a0cf48c383dc6cd9aebf50bf
Aaron Slaughter <aslaugh1@gmail.com>
2022-11-29 12:04:30 -0500

    Create components

---
+++ client-authentication-csr/app/components/EmptyList.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import styles from '../styles/Home.module.css'
+
+const EmptyList: NextPage = () => (
+  <div className={styles.main}>
+    <h1 className={styles.title}>Nothing Here 🧐</h1>
+    <p className={styles.description}>
+      Publish a &apos;Fun Fact&apos; in{' '}
+      <a href="http://localhost/cms" target="_blank" rel="noreferrer">
+        Brightspot
+      </a>
+    </p>
+  </div>
+)
+
+export default EmptyList
---
+++ client-authentication-csr/app/pages/_app.tsx
@@ -0,0 +1,24 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { ApolloProvider } from '@apollo/client'
+import { client } from '../lib/client'
+import Head from 'next/head'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ApolloProvider client={client}>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta
+          name="description"
+          content="CSR Client Authentication example with Brightspot"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="https://www.brightspot.com/favicon-32x32.png" />
+      </Head>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
+}
+
+export default MyApp
