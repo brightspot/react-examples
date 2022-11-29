2ac48dfa4839f85e85628822be8dfe1c5f113351
Aaron Slaughter <aslaugh1@gmail.com>
2022-11-29 12:08:40 -0500

    Create data model and view model

---
+++ client-authentication-ssr/brightspot/src/brightspot/example/client_authentication_ssr/FunFact.ts
@@ -0,0 +1,12 @@
+import JavaClass from 'brightspot-types/JavaClass'
+import JavaField from 'brightspot-types/JavaField'
+
+import Content from 'brightspot-types/com/psddev/cms/db/Content'
+
+export default class FunFact extends JavaClass(
+  'brightspot.example.client_authentication_ssr.FunFact',
+  Content
+) {
+  @JavaField(String)
+  text?: string
+}
