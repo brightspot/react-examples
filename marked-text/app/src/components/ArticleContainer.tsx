261a43843e76c53f06f7f1e4756f68ceb7c268a7
Roderick Cardenas <roderickcardenas1@gmail.com>
2022-12-07 16:53:29 -0500

    Refactor based on feedback

--- marked-text/app/src/components/ArticleContainer.tsx
+++ marked-text/app/src/components/ArticleContainer.tsx
@@ -1,27 +1,18 @@
 import { useState, useEffect, useCallback } from 'react'
 import { ArticleMarkQuery } from './ArticleMarkQuery'
-import { Mark, MarkedText } from '../brightspot-marked-text/types'
-import { markedTextTraversal } from '../brightspot-marked-text/index'
-import TypeComponentHandler from './TypeComponentHandler'
-import TextComponent from './TextComponent'
-import ReactComponent from './ReactComponent'
-// import { markedTextTraversal } from 'bsp-marked-text'
-
+import { MarkedText } from '../brightspot-marked-text/types'
+import { MarkedTextComponent } from './MarkedTextComponent'
 interface ArticleData {
   headline: string
   body: MarkedText
 }
-
 interface ArticleResponse {
   articleData?: ArticleData
   errors?: string[]
 }
-
-const ArticleContainer = () => {
+const Article = () => {
   const [article, setArticle] = useState<ArticleResponse>()
-
   const GRAPHQL_URL = process.env.REACT_APP_GRAPHQL_URL ?? ''
-
   const handleResponse = (res: any) => {
     let articleData: ArticleData | undefined
     let errors: string[] = []
@@ -33,19 +24,16 @@ const ArticleContainer = () => {
     }
     if (res.errors)
       res.errors.forEach((error: any) => errors.push(error.message))
-
     setArticle({
       articleData,
       errors: errors,
     })
   }
-
   const handleError = (error: Error) => {
     setArticle({
       errors: [error.message],
     })
   }
-
   const fetchArticleData = useCallback(async () => {
     const dataRequestParams = {
       method: 'POST',
@@ -59,43 +47,24 @@ const ArticleContainer = () => {
         },
       }),
     }
-
     await fetch(GRAPHQL_URL, dataRequestParams)
       .then((res) => res.json())
       .then((res) => handleResponse(res))
       .catch((error: Error) => handleError(error))
   }, [GRAPHQL_URL])
-
   useEffect(() => {
     fetchArticleData()
   }, [fetchArticleData])
 
-  //Called when arriving at the text within a mark
-  const textHandler = (text: string) => {
-    return <TextComponent key={text} text={text} />
-  }
-
-  //Called once all the text within the mark has been checked/handled
-  const componentHandler = (mark: Mark, children: Array<React.ReactElement>) =>
-    TypeComponentHandler(mark, children)
-
-  // Required object passed to markedTextTraversal
-  const visitorHandler = {
-    visitText: textHandler,
-    visitMark: componentHandler,
-  }
-
   return (
-    <div className="marked-text-container">
+    <div className="marked-text-container Article">
       <h1 className="headline">{article?.articleData?.headline}</h1>
-      {article?.articleData &&
-        markedTextTraversal(article?.articleData?.body, visitorHandler).map(
-          (Component, index: number) => {
-            return <ReactComponent key={index} Component={Component} />
-          }
+      <>
+        {article?.articleData?.headline && (
+          <MarkedTextComponent markedText={article.articleData.body} />
         )}
+      </>
     </div>
   )
 }
-
-export default ArticleContainer
+export default Article
