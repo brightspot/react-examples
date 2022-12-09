d43ec9ad1ffb211868539a5045d12305e1ded757
Roderick Cardenas <roderickcardenas1@gmail.com>
2022-12-09 13:04:10 -0500

    Update endpoint and article names

--- marked-text/brightspot/src/brightspot/example/marked_text/Article.ts
+++ marked-text/brightspot/src/brightspot/example/marked_text/Article.ts
@@ -3,6 +3,7 @@ import JavaField from 'brightspot-types/JavaField'
 
 import Content from 'brightspot-types/com/psddev/cms/db/Content'
 import DirectoryItem from 'brightspot-types/com/psddev/cms/db/Directory$Item'
+import DisplayName from 'brightspot-types/com/psddev/dari/db/Recordable$DisplayName'
 import Required from 'brightspot-types/com/psddev/dari/db/Recordable$Required'
 import RichText from 'brightspot-types/com/psddev/cms/db/ToolUi$RichText'
 import Site from 'brightspot-types/com/psddev/cms/db/Site'
@@ -10,6 +11,7 @@ import Utils from 'brightspot-types/com/psddev/dari/util/Utils'
 
 import CustomRichTextToolbar from './CustomRichTextToolbar'
 
+@DisplayName({ value: 'Article: Marked Text Intro' })
 export default class Article extends JavaClass(
   'brightspot.example.marked_text.Article',
   Content,
--- marked-text/brightspot/src/brightspot/example/marked_text/ArticleEndpoint.ts
+++ marked-text/brightspot/src/brightspot/example/marked_text/ArticleEndpoint.ts
@@ -4,13 +4,13 @@ import List from 'brightspot-types/java/util/List'
 
 import ContentDeliveryApiEndpoint from 'brightspot-types/com/psddev/graphql/cda/ContentDeliveryApiEndpoint'
 import ContentDeliveryEntryPointField from 'brightspot-types/com/psddev/graphql/cda/ContentDeliveryEntryPointField'
+import DisplayName from 'brightspot-types/com/psddev/dari/db/Recordable$DisplayName'
 import GraphQLApiAccessOption from 'brightspot-types/com/psddev/graphql/GraphQLApiAccessOption'
 import GraphQLApiAccessOptionImplicit from 'brightspot-types/com/psddev/graphql/GraphQLApiAccessOptionImplicit'
 import GraphQLCorsConfiguration from 'brightspot-types/com/psddev/graphql/GraphQLCorsConfiguration'
 import Singleton from 'brightspot-types/com/psddev/dari/db/Singleton'
 
 import ArticleViewModel from './ArticleViewModel'
-import DisplayName from 'brightspot-types/com/psddev/dari/db/Recordable$DisplayName'
 
 @DisplayName({ value: 'Marked Text: Articles' })
 export default class ArticleEndpoint extends JavaClass(
@@ -19,7 +19,9 @@ export default class ArticleEndpoint extends JavaClass(
   Singleton
 ) {
   getPaths(): JavaSet<string> {
-    return ['/graphql/delivery/articles'] as unknown as JavaSet<string>
+    return [
+      '/graphql/delivery/marked-text-intro/articles',
+    ] as unknown as JavaSet<string>
   }
 
   [`getQueryEntryFields()`](): List<ContentDeliveryEntryPointField> {
