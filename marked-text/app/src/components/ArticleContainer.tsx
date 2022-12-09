8b8f3564d4744e2bfc049c272b7b29917ea0c5b3
Roderick Cardenas <roderickcardenas1@gmail.com>
2022-12-09 11:40:06 -0500

    move to one file and remove lib folder

--- marked-text/app/src/components/ArticleContainer.tsx
+++ marked-text/app/src/components/ArticleContainer.tsx
@@ -1,7 +1,10 @@
-import { useState, useEffect, useCallback } from 'react'
-import { ArticleMarkQuery } from './ArticleMarkQuery'
-import { MarkedText } from '../brightspot-marked-text/types'
-import { MarkedTextComponent } from './MarkedTextComponent'
+import { useState, useEffect, useCallback, Fragment, ReactNode } from 'react'
+import {
+  HtmlElement,
+  MarkedText,
+  markedTextTraversal,
+} from '@brightspot/marked-text'
+import React from 'react'
 interface ArticleData {
   headline: string
   body: MarkedText
@@ -10,6 +13,46 @@ interface ArticleResponse {
   articleData?: ArticleData
   errors?: string[]
 }
+
+const ArticleMarkQuery = `
+query ArticleMarkQuery($path: String) {
+  Article(model: {path: $path}) {
+    headline
+    body {
+      text
+      marks {
+        start
+        end
+        descendants
+        data {
+          ... on RteHtmlElement {
+            __typename
+            name
+            attributes {
+              name
+              value
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`
+
+const attrSwitch = (attr: string) => {
+  switch (attr) {
+    case 'class':
+      return 'className'
+    case 'colspan':
+      return 'colSpan'
+    case 'rowspan':
+      return 'rowSpan'
+    default:
+      return attr
+  }
+}
+
 const Article = () => {
   const [article, setArticle] = useState<ArticleResponse>()
   const GRAPHQL_URL = process.env.REACT_APP_GRAPHQL_URL ?? ''
@@ -56,13 +99,30 @@ const Article = () => {
     fetchArticleData()
   }, [fetchArticleData])
 
+  let key = 0
+
   return (
     <div className="marked-text-container Article">
       <h1 className="headline">{article?.articleData?.headline}</h1>
-      {article?.articleData?.headline && (
-        <MarkedTextComponent markedText={article.articleData.body} />
-      )}
+      {article?.articleData &&
+        markedTextTraversal(article.articleData.body, {
+          visitText: (text) => <Fragment key={key++}>{text}</Fragment>,
+          visitMark: (mark, children: ReactNode[]) => {
+            const element = mark.data as HtmlElement
+            const attrs = element.attributes.reduce((a, b) => {
+              const n: string = attrSwitch(b.name)
+              return { ...a, [n]: b.value }
+            }, {})
+
+            return React.createElement(
+              element.name,
+              { ...attrs, key: `k-${key++}` },
+              children
+            )
+          },
+        })}
     </div>
   )
 }
+
 export default Article
