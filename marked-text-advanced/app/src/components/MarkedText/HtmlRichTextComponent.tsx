1e372532ec53b653f4f865f92969cb433ff7bdd2
Roderick Cardenas <roderickcardenas1@gmail.com>
2022-12-13 15:59:16 -0500

    remove Utils place functions within scope

--- marked-text-advanced/app/src/components/MarkedText/HtmlRichTextComponent.tsx
+++ marked-text-advanced/app/src/components/MarkedText/HtmlRichTextComponent.tsx
@@ -1,6 +1,5 @@
 import React, { ReactNode } from 'react'
 import { HtmlElement } from '@brightspot/marked-text'
-import { attrHandler, voidElements } from '../../utils'
 
 const HtmlRichTextComponent = ({
   element,
@@ -23,5 +22,77 @@ const HtmlRichTextComponent = ({
     isVoidElement ? null : children
   )
 }
+
+/**
+ * Function used to convert regular html strings to React convention
+ * @param k The key used in the {@link HtmlElement } attributes array
+ * @returns {string} The React camelCased key
+ */
+const attrKey = (k: string) => {
+  switch (k) {
+    case 'class':
+      return 'className'
+    case 'colspan':
+      return 'colSpan'
+    case 'rowspan':
+      return 'rowSpan'
+    case 'charset':
+      return 'charSet'
+    case 'frameborder':
+      return 'frameBorder'
+    case 'allowfullscreen':
+      return 'allowFullScreen'
+    default:
+      return k
+  }
+}
+
+/**
+ * Function to take the attributes array held within the {@link HtmlElement} and return an object for the React Element Props.
+ * Style is unique as the key will be style and one long string as the value, it needs to be broken into another object of key value pairs.
+ * @param element {@link HtmlElement}
+ * @returns Object with key value pairs, with key being the HTML element attribute e.g. href, src and value being the string.
+ * The style attribute returns as an object with key being style and value being an object of key value pairs.
+ */
+const attrHandler = (element: HtmlElement) => {
+  const { attributes } = element
+  return attributes.reduce((a, b) => {
+    const attr: string = attrKey(b.name)
+    const attrVal = b.value
+    if (attr === 'style') {
+      let regex = /([\w-]*)\s*:\s*([^;]*)/g
+      let match,
+        properties: styleProperties = {}
+      match = regex.exec(attrVal)
+      while ((match = regex.exec(attrVal)))
+        properties[match[1]] = match[2].trim()
+      return { ...a, [attr]: properties }
+    }
+    return { ...a, [attr]: attrVal }
+  }, {})
+}
+interface styleProperties {
+  [k: string]: string
+}
+
+/**
+ * Array list of Void Elements
+ */
+const voidElements = [
+  'area',
+  'base',
+  'br',
+  'col',
+  'embed',
+  'hr',
+  'img',
+  'input',
+  'keygen',
+  'link',
+  'meta',
+  'source',
+  'track',
+  'wbr',
+]
 
 export default HtmlRichTextComponent
--- marked-text-advanced/app/src/components/MarkedText/ImageRichTextComponent.tsx
+++ marked-text-advanced/app/src/components/MarkedText/ImageRichTextComponent.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { ImageRichTextElement } from '../../types'
-import { imgEntriesHandler } from '../../utils'
 
 const ImageRichTextComponent = ({
   markData,
@@ -11,15 +10,37 @@ const ImageRichTextComponent = ({
 
   let key = 0
 
-  const attrs = imgEntriesHandler(markData?.image?.entries, alt)
+  const attrs = imgAttrHandler(markData?.image?.entries, alt)
 
   return (
     <span className="image-rte-container">
-      {React.createElement('img', { ...attrs, key: `k-${key++}` })}
+      {React.createElement('img', {
+        className: 'image-rte',
+        ...attrs,
+        key: `k-${key++}`,
+      })}
       <span className="caption">{caption}</span>
       <span className="credit">{credit}</span>
     </span>
   )
+}
+
+/**
+ * Function takes in an array of {@link entries} and a string for the image's alt attribute.
+ * Adds the alt to the the array and then uses a reducer to return one object with key value pairs.
+ * @param entries Array of Objects with key value pairs
+ * @param alt Alt string from {@link ImageRichTextElement}
+ * @returns Object with key value pairs
+ */
+const imgAttrHandler = (
+  entries: { key: string; value: string }[],
+  alt: string | undefined
+) => {
+  entries.push({ key: 'alt', value: alt ? alt : '' })
+
+  return entries.reduce((a, b) => {
+    return { ...a, [b.key]: b.value }
+  }, {})
 }
 
 export default ImageRichTextComponent
