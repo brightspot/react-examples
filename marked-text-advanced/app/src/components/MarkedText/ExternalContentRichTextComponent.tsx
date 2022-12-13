61ca36afaae98c002cd6866fcb02aa59e3a56902
Roderick Cardenas <roderickcardenas1@gmail.com>
2022-12-13 16:02:03 -0500

    update classname

--- marked-text-advanced/app/src/components/MarkedText/ExternalContentRichTextComponent.tsx
+++ marked-text-advanced/app/src/components/MarkedText/ExternalContentRichTextComponent.tsx
@@ -9,7 +9,7 @@ const ExternalContentRichTextComponent = ({
   const { markedHtml, type, title } = markData
 
   return (
-    <div className="external-rich-text-video">
+    <div className="external-rich-text-content-container">
       <h3>{title}</h3>
       <h3>Type of Rich Text Element: {type}</h3>
       {<MarkedTextComponent markedText={markedHtml} />}
