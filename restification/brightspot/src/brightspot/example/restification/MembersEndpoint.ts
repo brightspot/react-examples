3a98c74ee1281d65b23a9fbae17384dda1542b11
Roderick Cardenas <roderickcardenas1@gmail.com>
2022-12-19 17:12:23 -0500

    Update Endpoint to V1

--- restification/brightspot/src/brightspot/example/restification/MembersEndpoint.ts
+++ restification/brightspot/src/brightspot/example/restification/MembersEndpoint.ts
@@ -1,9 +1,8 @@
-import Class from 'brightspot-types/java/lang/Class'
 import JavaClass from 'brightspot-types/JavaClass'
 import JavaSet from 'brightspot-types/java/util/Set'
 import List from 'brightspot-types/java/util/List'
 
-import ContentManagementApiEndpoint from 'brightspot-types/com/psddev/graphql/cma/ContentManagementApiEndpoint'
+import ContentManagementApiEndpointV1 from 'brightspot-types/com/psddev/graphql/cma/ContentManagementApiEndpointV1'
 import ContentManagementEntryPointField from 'brightspot-types/com/psddev/graphql/cma/ContentManagementEntryPointField'
 import DisplayName from 'brightspot-types/com/psddev/dari/db/Recordable$DisplayName'
 import GraphQLCorsConfiguration from 'brightspot-types/com/psddev/graphql/GraphQLCorsConfiguration'
@@ -14,7 +13,7 @@ import Member from './Member'
 @DisplayName({ value: 'Members API: Restification' })
 export default class MembersEndpoint extends JavaClass(
   'brightspot.example.restification.MembersEndpoint',
-  ContentManagementApiEndpoint,
+  ContentManagementApiEndpointV1,
   Singleton
 ) {
   [`getEntryFields()`](): List<ContentManagementEntryPointField> {
