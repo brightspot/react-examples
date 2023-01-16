45186d1515bc05daedbc120884446aade88b2c9c
Roderick Cardenas <roderickcardenas1@gmail.com>
2023-01-16 16:25:24 -0500

    Add fetch data code with header

--- cors-configuration/app/src/App.tsx
+++ cors-configuration/app/src/App.tsx
@@ -1,9 +1,73 @@
 import './App.css'
+import { useState, useEffect } from 'react'
 
-function App() {
+interface MemoData {
+  AllMemos:
+    | {
+        memos: {
+          message: string
+          subject: string
+        }[]
+      }
+    | undefined
+}
+
+const App = () => {
+  const [memoData, setMemoData] = useState<MemoData>()
+
+  useEffect(() => {
+    const myHeaders = new Headers()
+    myHeaders.append('Foo', 'Bar')
+
+    const graphqlQuery = JSON.stringify({
+      query: `query MyQuery {
+        AllMemos {
+          memos {
+            message
+            subject
+          }
+        }
+      }`,
+    })
+    const requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: graphqlQuery,
+    }
+
+    const dataFetch = async () => {
+      const data = await fetch(
+        'http://localhost/graphql/delivery/cors-configuration',
+        requestOptions
+      )
+        .then((response) => response.json())
+        .catch((error) => console.log('error', error))
+
+      setMemoData(data.data)
+    }
+
+    dataFetch()
+  }, [])
+
+  if (!memoData)
+    return (
+      <div className="App">
+        <h1>Loading...</h1>
+      </div>
+    )
+
   return (
     <div className="App">
-      <h1>Sample App</h1>
+      <h1>CORS Configuration</h1>
+      <div className="seperator"></div>
+      {memoData?.AllMemos?.memos.map((memo, index) => {
+        return (
+          <div className="memo-card" key={index}>
+            <h2>{memo.subject}</h2>
+            <p>{memo.message}</p>
+          </div>
+        )
+      })}
     </div>
   )
 }
