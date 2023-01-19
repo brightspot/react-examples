6f1d9a221a92e683eb4d63195e4b1b32dfdbbc7b
Mandi Haase <haase1020@gmail.com>
2023-01-19 16:02:40 -0500

    update brightspot with current imports and update yarn lock

--- file-uploads/brightspot/src/brightspot/example/file_uploads/FileUploadsEndpoint.ts
+++ file-uploads/brightspot/src/brightspot/example/file_uploads/FileUploadsEndpoint.ts
@@ -1,8 +1,7 @@
-import Class from 'brightspot-types/java/lang/Class'
 import JavaSet from 'brightspot-types/java/util/Set'
 import List from 'brightspot-types/java/util/List'
 
-import ContentManagementApiEndpoint from 'brightspot-types/com/psddev/graphql/cma/ContentManagementApiEndpoint'
+import ContentManagementApiEndpointV1 from 'brightspot-types/com/psddev/graphql/cma/ContentManagementApiEndpointV1'
 import ContentManagementEntryPointField from 'brightspot-types/com/psddev/graphql/cma/ContentManagementEntryPointField'
 import GraphQLCorsConfiguration from 'brightspot-types/com/psddev/graphql/GraphQLCorsConfiguration'
 import JavaClass from 'brightspot-types/JavaClass'
@@ -12,12 +11,12 @@ import Image from './Image'
 
 export default class NotesEndpoint extends JavaClass(
   'brightspot.example.file_uploads.FileUploadsEndpoint',
-  ContentManagementApiEndpoint,
+  ContentManagementApiEndpointV1,
   Singleton
 ) {
   [`getEntryFields()`](): List<ContentManagementEntryPointField> {
     let entryPointField = new ContentManagementEntryPointField(
-      Image.class as Class<Image>,
+      Image.getClass(),
       true
     )
 
