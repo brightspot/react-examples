527337bb61e8d36f95335a837b425074743c7ef6
Mandi Haase <haase1020@gmail.com>
2023-02-06 11:45:51 -0500

    clean up interface

--- test/brightspot/src/brightspot/example/test/CourseModuleView.ts
+++ test/brightspot/src/brightspot/example/test/CourseModuleView.ts
@@ -4,10 +4,4 @@ import JavaMethodReturn from 'brightspot-types/JavaMethodReturn'
 
 export default abstract class CourseModuleView extends JavaInterface(
   'brightspot.example.test.CourseModuleView'
-) {
-  @JavaMethodParameters(String)
-  @JavaMethodReturn(String)
-  foo(x: string): string {
-    return ''
-  }
-}
+) {}
