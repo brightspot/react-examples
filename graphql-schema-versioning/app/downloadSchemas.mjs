d80fac7eea90a2bf1fa4133faba4f96e0c2f1fd1
Roderick Cardenas <roderickcardenas1@gmail.com>
2023-04-18 11:29:43 -0400

    Update app to make call to movie endpoint

--- graphql-schema-versioning/app/downloadSchemas.mjs
+++ graphql-schema-versioning/app/downloadSchemas.mjs
@@ -5,30 +5,41 @@ import { timeStamp } from './schemas/timestamp.mjs'
 
 dotenv.config()
 
+const MOVIES_URL = process.env.MOVIES_URL
 const SCHEMA_URL = process.env.GRAPHQL_SCHEMA_URL
 
+const allMoviesQuery = `
+  query Movies {
+    AllMovies {
+      movies {
+        title
+      }
+    }
+  }
+`
+
 const graphqlSchemaQuery = `
-    query Schemas {
-      versions: com_psddev_graphql_GraphQLSchemaVersionQuery(
-        where: { predicate: "endpoint/getLabel = ?", arguments: "Schema Versioning Movie Endpoint" }
-        sorts: { order: descending, options: "timestamp" }
-      ) {
-        items {
+  query Schemas {
+    versions: com_psddev_graphql_GraphQLSchemaVersionQuery(
+      where: { predicate: "endpoint/getLabel = ?", arguments: "Schema Versioning Movie Endpoint" }
+      sorts: { order: descending, options: "timestamp" }
+    ) {
+      items {
+        _id
+        _type
+        endpoint {
           _id
-          _type
-          endpoint {
-            _id
-          }
-          release
-          schemaHash
-          timestamp
-          schema {
-            publicUrl
-          }
+        }
+        release
+        schemaHash
+        timestamp
+        schema {
+          publicUrl
         }
       }
     }
-  `
+  }
+`
 
 const downloadSchemas = async (schemasToDownload) => {
   await schemasToDownload.forEach((schema) => {
@@ -68,4 +79,16 @@ const fetchSchemas = async (url) => {
   parseSchemaURLS(schemas)
 }
 
-fetchSchemas(SCHEMA_URL)
+const schemaLoadAndFetch = async (moviesURL, schemaURL) => {
+  await axios({
+    method: 'post',
+    url: moviesURL,
+    data: {
+      query: allMoviesQuery,
+    },
+    redirect: 'follow',
+  }).then((res) => console.log(res.data.data))
+  fetchSchemas(schemaURL)
+}
+
+schemaLoadAndFetch(MOVIES_URL, SCHEMA_URL)
