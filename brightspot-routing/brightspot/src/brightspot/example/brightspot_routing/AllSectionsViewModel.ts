57343ec148d5c77247a20a4bfa9de46406b03a3c
Aaron Slaughter <aslaugh1@gmail.com>
2023-05-08 11:17:13 -0400

    Gets results only from current site

--- brightspot-routing/brightspot/src/brightspot/example/brightspot_routing/AllSectionsViewModel.ts
+++ brightspot-routing/brightspot/src/brightspot/example/brightspot_routing/AllSectionsViewModel.ts
@@ -3,8 +3,10 @@ import JavaMethodParameters from 'brightspot-types/JavaMethodParameters'
 import JavaMethodReturn from 'brightspot-types/JavaMethodReturn'
 import List from 'brightspot-types/java/util/List'
 
+import CurrentSite from 'brightspot-types/com/psddev/cms/page/CurrentSite'
 import DirectoryStatic from 'brightspot-types/com/psddev/cms/db/Directory$Static'
 import Query from 'brightspot-types/com/psddev/dari/db/Query'
+import Site from 'brightspot-types/com/psddev/cms/db/Site'
 import ViewInterface from 'brightspot-types/com/psddev/cms/view/ViewInterface'
 import ViewModel from 'brightspot-types/com/psddev/cms/view/ViewModel'
 
@@ -17,14 +19,27 @@ export default class AllSectionsViewModel extends JavaClass(
   'brightspot.example.brightspot_routing.AllSectionsViewModel',
   ViewModel.Of(BrightspotRoutingEndpoint)
 ) {
+  @CurrentSite
+  site?: Site
+
   @JavaMethodParameters()
   @JavaMethodReturn(List.Of(SectionViewModel))
   getSections(): List<SectionViewModel> {
+    let query = Query.from(Section.getClass())
+    
+    query.where(DirectoryStatic.hasPathPredicate())
+    
+    if (this.site) {
+      query.and(this.site.itemsPredicate())
+    } else {
+      query.and('cms.site.owner = missing')
+    }
+
+    let sections = query.selectAll()
+
     return this.createViews(
       SectionViewModel.getClass(),
-      Query.from(Section.getClass())
-        .where(DirectoryStatic.hasPathPredicate())
-        .selectAll()
-    ) as List<SectionViewModel>
+      sections
+    )
   }
 }
