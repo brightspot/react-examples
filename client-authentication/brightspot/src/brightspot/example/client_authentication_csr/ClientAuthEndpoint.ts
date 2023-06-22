69f5efa2ef2ef15eae785d0964e6d55edd4d368d
Aaron Slaughter <aslaugh1@gmail.com>
2023-06-22 10:05:27 -0400

    Adds display name

--- client-authentication/brightspot/src/brightspot/example/client_authentication_csr/ClientAuthEndpoint.ts
+++ client-authentication/brightspot/src/brightspot/example/client_authentication_csr/ClientAuthEndpoint.ts
@@ -4,6 +4,7 @@ import List from 'brightspot-types/java/util/List'
 
 import ContentDeliveryApiEndpointV1 from 'brightspot-types/com/psddev/graphql/cda/ContentDeliveryApiEndpointV1'
 import ContentDeliveryEntryPointField from 'brightspot-types/com/psddev/graphql/cda/ContentDeliveryEntryPointField'
+import DisplayName from 'brightspot-types/com/psddev/dari/db/Recordable$DisplayName'
 import GraphQLApiAccessOption from 'brightspot-types/com/psddev/graphql/GraphQLApiAccessOption'
 import GraphQLApiAccessOptionExplicit from 'brightspot-types/com/psddev/graphql/GraphQLApiAccessOptionExplicit'
 import GraphQLCorsConfiguration from 'brightspot-types/com/psddev/graphql/GraphQLCorsConfiguration'
@@ -11,6 +12,7 @@ import Singleton from 'brightspot-types/com/psddev/dari/db/Singleton'
 
 import AllFunFactsViewModel from './AllFunFactsViewModel'
 
+@DisplayName({ value: 'Fun Fact Endpoint' })
 export default class ClientAuthEndpoint extends JavaClass(
   'brightspot.example.client_authentication.ClientAuthEndpoint',
   ContentDeliveryApiEndpointV1,
